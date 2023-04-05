59b699fd5aeb6a0ceeb5c7bba5e084e77d6aa820
Anurag Sharma <mrshanksster@gmail.com>
2023-04-04 20:59:01 -0500

    feat: add go plugin and fmt

--- plugins.lua
+++ plugins.lua
@@ -54,20 +54,20 @@ local plugins = {
   {
     "ThePrimeagen/refactoring.nvim",
   },
-  -- {
-  --   "ray-x/go.nvim",
-  --   dependencies = {  -- optional packages
-  --     "ray-x/guihua.lua",
-  --     "neovim/nvim-lspconfig",
-  --     "nvim-treesitter/nvim-treesitter",
-  --   },
-  --   config = function()
-  --     require("go").setup()
-  --   end,
-  --   event = {"CmdlineEnter"},
-  --   ft = {"go", 'gomod'},
-  --   build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-  -- }
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  }
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
